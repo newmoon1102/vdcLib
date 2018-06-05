230af2926084fb829eefafaf8b2e7be768708a16
newmoon1102 <dinhchung.utc.k49@gmail.com>
2018-06-05 10:30:55 +0900

    Add files via upload

---
+++ BoxedAppSDK/BoxedAppSDK/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace BoxedAppSDK
+{
+    public class Develop
+    {
+        [DllImport("BoxedAppSDK.Develop.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool BoxedAppSDK_Init();
+        [DllImport("BoxedAppSDK.Develop.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool BoxedAppSDK_Exit();
+        [DllImport("BoxedAppSDK.Develop.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr BoxedAppSDK_CreateVirtualFileBasedOnIStream(string lpFileName, EFileAccess dwDesiredAccess, EFileShare dwShareMode, IntPtr lpSecurityAttributes, ECreationDisposition dwCreationDisposition, EFileAttributes dwFlagsAndAttributes, IntPtr hTemplateFile, IStream Stream);
+
+        [Flags]
+        public enum EFileAccess : uint
+        {
+            GenericAll = 268435456,
+            GenericExecute = 536870912,
+            GenericWrite = 1073741824,
+            GenericRead = 2147483648
+        }
+        [Flags]
+        public enum EFileShare : uint
+        {
+            None = 0,
+            Read = 1,
+            Write = 2,
+            Delete = 4
+        }
+        public enum ECreationDisposition : uint
+        {
+            New = 1,
+            CreateAlways = 2,
+            OpenExisting = 3,
+            OpenAlways = 4,
+            TruncateExisting = 5
+        }
+        [Flags]
+        public enum EFileAttributes : uint
+        {
+            Readonly = 1,
+            Hidden = 2,
+            System = 4,
+            Directory = 16,
+            Archive = 32,
+            Device = 64,
+            Normal = 128,
+            Temporary = 256,
+            SparseFile = 512,
+            ReparsePoint = 1024,
+            Compressed = 2048,
+            Offline = 4096,
+            NotContentIndexed = 8192,
+            Encrypted = 16384,
+            FirstPipeInstance = 524288,
+            OpenNoRecall = 1048576,
+            OpenReparsePoint = 2097152,
+            PosixSemantics = 16777216,
+            BackupSemantics = 33554432,
+            DeleteOnClose = 67108864,
+            SequentialScan = 134217728,
+            RandomAccess = 268435456,
+            NoBuffering = 536870912,
+            Overlapped = 1073741824,
+            Write_Through = 2147483648
+        }
+    }
+}
---
+++ VDCLibarary/VDCLibarary/Libarary.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VDCSDK
+{
+    public sealed class App
+    {
+        public static int ChunkSize = 1024 * 1024;
+        private static string userName;
+        private static string emailFrom;
+        private static string emailTo;
+        private static string pass;
+        const string userRoot = "HKEY_CURRENT_USER";
+        const string subkey = "PVEAPP";
+        const string keyName = userRoot + "\\" + subkey;
+
+        public static void AppSDK_Init(string key)
+        {
+            try
+            {
+                string encryptEmail = DecryptKey(key, true);
+                emailFrom = encryptEmail.Split('/')[0];
+                pass = encryptEmail.Split('/')[1];
+                emailTo = encryptEmail.Split('/')[2];
+                userName = emailFrom.Split('@')[0];
+
+            } catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public static void AppSDK_Init(string mailFrom, string passMail, string mailTo)
+        {
+            try
+            {
+                emailFrom = mailFrom;
+                pass = passMail;
+                emailTo = mailTo;
+                userName = emailFrom.Split('@')[0];
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        /// <summary>EncryptVideo</summary>
+        /// <param name="input"></param>
+        /// <param name="Key"></param>
+        /// <param name="IV"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static byte[] EncryptVideo(byte[] input, byte[] Key, byte[] IV, int size)
+        {
+            byte[] output;
+
+            TripleDESCryptoServiceProvider TDES = new TripleDESCryptoServiceProvider();
+
+            using (MemoryStream EncryptedDataStream = new MemoryStream())
+            {
+                using (CryptoStream CryptoStream = new CryptoStream(EncryptedDataStream, TDES.CreateEncryptor(Key, IV), CryptoStreamMode.Write))
+                {
+                    CryptoStream.Write(input, 0, size);
+                    CryptoStream.FlushFinalBlock();
+
+                    output = new byte[EncryptedDataStream.Length];
+                    EncryptedDataStream.Position = 0;
+                    EncryptedDataStream.Read(output, 0, output.Length);
+                }
+            }
+
+            return output;
+        }
+        /// <summary>DecryptVideo</summary>
+        /// <param name="input"></param>
+        /// <param name="Key"></param>
+        /// <param name="IV"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static byte[] DecryptVideo(byte[] input, byte[] Key, byte[] IV, int size)
+        {
+            byte[] output = new byte[size];
+
+            TripleDESCryptoServiceProvider TDES = new TripleDESCryptoServiceProvider();
+
+            using (MemoryStream EncryptedDataStream = new MemoryStream(input))
+            {
+                using (CryptoStream CryptoStream = new CryptoStream(EncryptedDataStream, TDES.CreateDecryptor(Key, IV), CryptoStreamMode.Read))
+                {
+                    CryptoStream.Read(output, 0, size);
+                }
+            }
+
+            return output;
+        }
+
+        /// <summary>EncryptKey</summary>
+        /// <param name="strEncrypt"></param>
+        /// <param name="useHashing"></param>
+        /// <returns></returns>
+        public static string EncryptKey(string strEncrypt, bool useHashing)
+        {
+            try
+            {
+                byte[] keyArray;
+                byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(strEncrypt);
+
+                AppSettingsReader settingsReader = new AppSettingsReader();
+
+                if (useHashing)
+                {
+                    MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                    keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(subkey));
+                    hashmd5.Clear();
+                }
+                else
+                    keyArray = UTF8Encoding.UTF8.GetBytes(subkey);
+
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = tdes.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                tdes.Clear();
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>DecryptKey</summary>
+        /// <param name="strCipher"></param>
+        /// <param name="useHashing"></param>
+        /// <returns></returns>
+        public static string DecryptKey(string strCipher, bool useHashing)
+        {
+            try
+            {
+                byte[] keyArray;
+                byte[] toEncryptArray = Convert.FromBase64String(strCipher);
+
+                AppSettingsReader settingsReader = new AppSettingsReader();
+
+                if (useHashing)
+                {
+                    MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                    keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(subkey));
+                    hashmd5.Clear();
+                }
+                else
+                    keyArray = UTF8Encoding.UTF8.GetBytes(subkey);
+
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = tdes.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                tdes.Clear();
+                return UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>GetMd5Hash</summary>
+        /// <param name="md5Hash"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            try
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        /// <summary>Verify a hash against a string.</summary>
+        /// <param name="md5Hash"></param>
+        /// <param name="input"></param>
+        /// <param name="hash"></param>
+        /// <returns></returns>
+        public static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool RegisterKey(string key, string value)
+        {
+            try
+            {
+                Microsoft.Win32.Registry.SetValue(keyName, key, value);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string GetValueOfKey(string key)
+        {
+            try
+            {
+                string value = (string)Microsoft.Win32.Registry.GetValue(keyName,
+                 key, null);
+
+                return value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool SendMail(string mailcc,string subject, string body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(emailFrom);
+                mail.To.Add(emailTo);
+
+                if (!String.IsNullOrEmpty(mailcc))
+                {
+                    mail.CC.Add(mailcc);
+                }
+                mail.Subject = subject;
+                mail.Body = body;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(userName, pass);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
